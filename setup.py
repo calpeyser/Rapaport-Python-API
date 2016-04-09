549af84fb9f6b5921843d7cb365d161a079b63b2
calpeyser <calpeyser@gmail.com>
2016-04-09 15:00:02 -0400

    setup installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = "Rapaport-Python-API",
+	version = "1.0",
+	description = "A Python library for accessing to Rapaport TechNet API",
+	Author = "Cal Peyser",
+	packages = [
+		"Rapaport"
+	]
+)
